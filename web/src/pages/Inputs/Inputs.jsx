4930d22d46e7d0e891f69620f912a55a58f92514
Hiaguedes <hiago_guedes94@hotmail.com>
2020-10-27 19:50:45 -0300

    fazendo validação básica dos inputs

--- web/src/pages/Inputs/Inputs.jsx
+++ web/src/pages/Inputs/Inputs.jsx
@@ -14,11 +14,35 @@ export default function Inputs(){
         <>
         <Form className="form" variant="light" onSubmit={handleSubmit(onSubmit)}>
             <Form.Label>Nome Completo:</Form.Label>
-            <Form.Control name="nome" className="input-form" type="text" ref={register({required: true})}/>
+            <Form.Control name="nome" className="input-form" type="text" ref={register(
+                {
+                    required: {
+                        value: true,
+                        message: 'Favor preencher o campo'
+                    }
+                    
+                    
+                    })}/>
+            <Form.Text className="error-text w-100">{errors?.nome?.message}</Form.Text>
 
             <Form.Label>Idade:</Form.Label>
-            <Form.Control name="idade" className="input-form" type="number" ref={register({min:5,max:120})}/>
-            <Form.Text>{errors.idade?.message}</Form.Text>
+            <Form.Control name="idade" className="input-form" type="number" ref={register(
+                {
+                    min:{
+                        value: 5,
+                        message: 'A idade mínima é de 5 anos'
+                    },
+                    max:{
+                        value: 120,
+                        message: 'A idade máxima é de 120 anos'
+                    },
+                    required: {
+                        value:true,
+                        message: 'Favor preencher o campo'
+                    }
+                    
+                    })}/>
+            <Form.Text className="error-text w-100">{errors?.idade?.message}</Form.Text>
 
             <Form.Label>Estado Civil:</Form.Label>
             <Form.Control name="estadoCivil" as="select" className="input-form" type="text" ref={register({required: true})}>
@@ -30,13 +54,37 @@ export default function Inputs(){
             </Form.Control>
 
             <Form.Label>CPF:</Form.Label>
-            <Form.Control name="cpf" className="input-form" type="number" ref={register({required: true})}/>
+            <Form.Control name="cpf" className="input-form" type="number" ref={
+                register(
+                {
+                    required: {
+                        value: true,
+                        message: 'Favor preencher o campo'
+            }
+            
+            })
+            }/>
+            <Form.Text className="error-text w-100">{errors?.cpf?.message}</Form.Text>
+            
 
             <Form.Label>Cidade:</Form.Label>
-            <Form.Control name="cidade" className="input-form" type="text" ref={register({required: true})}/>
+            <Form.Control name="cidade" className="input-form" type="text" ref={register({
+                required: {
+                    value: true,
+                    message: 'Favor preencher o campo'
+                }
+                })}/>
+
+            <Form.Text className="error-text w-100">{errors?.cidade?.message}</Form.Text>
 
             <Form.Label>Estado:</Form.Label>
-            <Form.Control name="estado" className="input-form" type="text" ref={register({required: true})}/>
+            <Form.Control name="estado" className="input-form" type="text" ref={register(
+                {required: {
+                    value: true,
+                    message: 'Favor preencher o campo'
+                }
+                })}/>
+            <Form.Text className="error-text w-100">{errors?.estado?.message}</Form.Text>
 
             <Button className="mt-4" variant="primary" type="submit">Adicionar Dado</Button>
         </Form>
